4c3ce3fb67143a27c6f66020045ae6df8491d75b
NwankwoKc <84792410+NwankwoKc@users.noreply.github.com>
2021-05-27 17:11:34 +0100

    Add files via upload
    we were just learning how to commit things to git directory

---
+++ Blog.js
@@ -0,0 +1,49 @@
+// let bp = document.getElementById('blogpost');
+// bd = document.getElementById('body');
+// let sm = document.getElementById('secmain');
+// let obj = [{
+//     id:1,
+//     title:"How to make Akara",
+//     authur:"Johnleonard UC",
+//     context: "lueren ipsum lueren ipsum lueren ipsum lueren ipsum lueren ipsum lueren ipsum lueren ipsum",
+//     tag:"food"
+// },
+// {
+//     id:2,
+//     title:"How to use html 5 canvas",
+//     authur:"Lucky Eli",
+//     context:"luern ipsum lueren ipsum lueren ipsum lueren ipsum lueren ipsumlueren ipsum lueren ipsum lueren ipsum lueren ipsum",
+//     tag:"paragragph"
+// }];
+// // let gg = document.getElementById('body')
+
+// function blog(a,b,c,d,e,f,g,h,i,j,k,l,m){
+//     let tag1 = document.getElementById(a);
+//     let tag2 = document.createElement(b);
+//     let tag3 = document.createElement(c);
+//     tag3.innerHTML = d;
+//     let tag4 = document.createElement(e);
+//     tag4.innerHTML = f;
+//     let tag5 = document.createElement(g);
+//     tag5.innerHTML = h;
+//     let tag6 = document.createElement(i);
+//     let tag7 = document.createElement(j);
+//     tag7.innerHTML = k;
+//     let tag8 = document.createElement(l);
+//     tag8.innerHTML = m;
+
+//     tag2.appendChild(tag3);
+//     tag2.appendChild(tag4);
+//     tag2.appendChild(tag5);
+//     tag6.appendChild(tag7);
+//     tag6.appendChild(tag8);
+//     tag2.appendChild(tag6);
+//     tag1.appendChild(tag2);
+// }
+
+// for(let i=0; i<obj.length; i++) {
+//     let lo = obj[i];
+//     blog('body','section','p', lo.id,'h2',lo.title,'p',lo.context,'div','p',lo.tag,'p',lo.authur);
+// }
+
+// arrow function
